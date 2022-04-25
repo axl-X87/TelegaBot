161d0b635cae8dbc34e181233ad8736969d8cb6b
axl-X87 <73424809+axl-X87@users.noreply.github.com>
2022-04-25 12:50:24 +0300

    Добавьте файлы проекта.

---
+++ TelegaBot/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Exceptions;
+
+
+namespace TelegaBot
+{
+
+    public class Program
+    {
+        public static ITelegramBotClient bot = new TelegramBotClient(Token.tokenBot);
+        public static async Task HandleUpdateAsync(ITelegramBotClient telegram, Update update, CancellationToken token)
+        {            
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                Message message = update.Message;
+                if (message.Text[0] == '/')
+                {
+                    Console.WriteLine("Bot cath command : " + message.Text);
+                }
+                else
+                {
+                    Console.WriteLine("Bot cath message : " + message.Text);
+                }
+                TextCommands.GetMessangeCommand(telegram, message, message.Text);
+            }
+            else if (update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery)
+            {                
+                Message message = update.CallbackQuery.Message;
+                Console.WriteLine("Bot cath inline command : " + message.Text);
+                InlineCommands.CallBack(telegram, update, message);
+            }
+        }
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Ошибка Бота");
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bot started " + bot.GetMeAsync().Result.FirstName);
+            var cts = new CancellationTokenSource();
+            var ctsToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { },
+            };
+            bot.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, ctsToken);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TelegaBot/TextCommands.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Exceptions;
+
+namespace TelegaBot
+{
+    public static class TextCommands
+    {
+        static bool active = false;
+        public static void GetMessangeCommand(ITelegramBotClient telegram, Message message, string command)
+        {
+            if (command[0] == '/')
+            {
+                if (command == "/menuBot")
+                {
+                    telegram.SendTextMessageAsync(message.Chat, "/StartAction\n/StopAction\n/FuckYou\n/OhShitImSorry");
+                }
+                else if (command == "/StartAction")
+                {
+                    active = true;
+                    telegram.SendTextMessageAsync(message.Chat, "Begin work bot!");
+                }
+                else if (command == "/StopAction")
+                {
+                    active = false;
+                    telegram.SendTextMessageAsync(message.Chat, "Stop work bot!");
+                }
+                else if (command == "/FuckYou")
+                {
+                    if (active == true)
+                    {
+                        telegram.SendTextMessageAsync(message.Chat, "No, Fuck You Leather Man", replyMarkup: InlineCommands.GetKeyboard("fuck"));
+                    }
+                    else
+                    {
+                        telegram.SendTextMessageAsync(message.Chat, "Bot is sleep");
+                    }
+                }
+                else if (command == "/null")
+                {
+                    if (active == true)
+                    {
+                        telegram.SendTextMessageAsync(message.Chat, "KAL", replyMarkup: InlineCommands.GetDBInline());
+                    }
+                    else
+                    {
+                        telegram.SendTextMessageAsync(message.Chat, "Bot is sleep");
+                    }
+                }
+                else if (command == "/Kal")
+                {
+                    if (active == true)
+                    {
+                        telegram.SendTextMessageAsync(message.Chat, "Kal?", replyMarkup: InlineCommands.GetKeyboard("kal"));
+                    }
+                    else
+                    {
+                        telegram.SendTextMessageAsync(message.Chat, "Bot is sleep");
+                    }
+                }
+                else if (command == "/OhShitImSorry")
+                {
+                    if (active == true)
+                    {
+                        telegram.SendTextMessageAsync(message.Chat, "Sorry for what?\nOur dad learn us not shame our dicks, exacly when he a such a good size.");
+                    }
+                    else
+                    {
+                        telegram.SendTextMessageAsync(message.Chat, "Bot is sleep");
+                    }
+                }
+                else
+                {
+                    telegram.SendTextMessageAsync(message.Chat, "Incorect command");
+                }
+            }
+            else
+            {
+                telegram.SendTextMessageAsync(message.Chat, "good to say!");
+            }
+        }
+    }
+}
